5ee0aa779c10f960c2477506e496a6b28b842249
DANR1BEIRO <danribeirodev@gmail.com>
2026-02-14 11:45:43 -0300

    study: 07 - RequestMapping e Servlet Context

--- anime-service/src/main/java/academy/devdojo/controllers/HelloController.java
+++ anime-service/src/main/java/academy/devdojo/controllers/HelloController.java
@@ -1,9 +1,11 @@
 package academy.devdojo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+// @RequestMapping( "greetings") A request at class level
 public class HelloController {
 
     @GetMapping("hi")
