63b9107033ceb6cf8a22bda9e131d1bdbe94c517
DANR1BEIRO <danribeirodev@gmail.com>
2026-02-15 11:11:24 -0300

    study:  09 - Exercício 01 - GetMapping

--- anime-service/src/main/java/academy/devdojo/AnimeServiceApplication.java
+++ anime-service/src/main/java/academy/devdojo/AnimeServiceApplication.java
@@ -2,23 +2,12 @@ package academy.devdojo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"outside.devdojo", "academy.devdojo"})
 public class AnimeServiceApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-        var applicationContext = SpringApplication.run(AnimeServiceApplication.class, args);
-
-		Arrays.stream(applicationContext.getBeanDefinitionNames())
-				.forEach(System.out::println);
-
-	}
-
+        SpringApplication.run(AnimeServiceApplication.class, args);
+    }
 }
