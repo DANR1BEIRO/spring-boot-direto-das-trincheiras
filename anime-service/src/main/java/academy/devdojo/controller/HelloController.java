9676dcba8666c9124ebaa5c490ced8ecbdf5ab35
DANR1BEIRO <danribeirodev@gmail.com>
2026-02-19 18:01:12 -0300

    study: 14 - PostMapping

--- anime-service/src/main/java/academy/devdojo/controller/HelloController.java
+++ anime-service/src/main/java/academy/devdojo/controller/HelloController.java
@@ -1,14 +1,29 @@
 package academy.devdojo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
+@RequestMapping("v1/greetings")
+@Slf4j
 // @RequestMapping( "greetings") A request at class level
 public class HelloController {
 
-    @GetMapping("hi")
+    @GetMapping()
     public String hi() {
         return "OMAE WA MOU SHINDE IRU";
+    }
+
+
+    // @PostMapping is a Spring annotation that maps HTTP POST requests to a specific handler method in a controller.
+    // @RequestBody is used to bind the request body to a method parameter.
+    // It tells Spring to deserialize the incoming JSON (or other format) into a Java object.
+    // In this case, it binds the request body to the 'name' parameter of type String.
+    @PostMapping
+    public Long save(@RequestBody String name) {
+        log.info("save '{}'", name);
+        return ThreadLocalRandom.current().nextLong(1, 1000);
     }
 }
