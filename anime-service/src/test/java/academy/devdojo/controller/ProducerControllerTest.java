febd7ff5924136b872004d58070953182b41c272
DANR1BEIRO <danribeirodev@gmail.com>
2026-03-10 13:41:20 -0300

    study: 42 - Unit Test - Controller 02

--- anime-service/src/test/java/academy/devdojo/controller/ProducerControllerTest.java
+++ anime-service/src/test/java/academy/devdojo/controller/ProducerControllerTest.java
@@ -1,29 +1,31 @@
 package academy.devdojo.controller;
 
 import academy.devdojo.domain.Producer;
-import academy.devdojo.mapper.ProducerMapperImpl;
 import academy.devdojo.repository.ProducerData;
-import academy.devdojo.repository.ProducerHardCodedRepository;
-import academy.devdojo.service.ProducerService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 @WebMvcTest(controllers = ProducerController.class)
-@Import({ProducerMapperImpl.class, ProducerService.class, ProducerHardCodedRepository.class, ProducerData.class})
+@ComponentScan(basePackages = "academy.devdojo")
 class ProducerControllerTest {
 
     @Autowired
@@ -32,12 +34,18 @@ class ProducerControllerTest {
     @MockitoBean
     private ProducerData producerData;
     private List<Producer> producerList;
+    @Autowired
+    private ResourceLoader resourceLoader; // utilizado para carregar um arquivo (classes dentro do resource)
 
     @BeforeEach
     void init() {
-        var ufotable = Producer.builder().id(1L).name("Ufotable").createdAt(LocalDateTime.now()).build();
-        var toeyAnimation = Producer.builder().id(2L).name("ToeyAnimation").createdAt(LocalDateTime.now()).build();
-        var ghibli = Producer.builder().id(3L).name("Studio Ghibli").createdAt(LocalDateTime.now()).build();
+        String dateTime = "2026-03-10T13:22:55.567057265";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS");
+        LocalDateTime localDateTime = LocalDateTime.parse(dateTime, formatter);
+
+        var ufotable = Producer.builder().id(1L).name("Ufotable").createdAt(localDateTime).build();
+        var toeyAnimation = Producer.builder().id(2L).name("ToeyAnimation").createdAt(localDateTime).build();
+        var ghibli = Producer.builder().id(3L).name("Studio Ghibli").createdAt(localDateTime).build();
         producerList = new ArrayList<>(List.of(ufotable, toeyAnimation, ghibli));
     }
 
@@ -45,10 +53,25 @@ class ProducerControllerTest {
     @DisplayName("findAll returns a list with all producerList")
     void findAll_ReturnAllProducers_WhenSuccesful() throws Exception {
         BDDMockito.when(producerData.getProducers()).thenReturn(producerList);
+        String response = readResourceFile("producer/get-producer-null-name-200.json");
 
         mockMvc.perform(MockMvcRequestBuilders.get("/v1/producers"))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].id").value(1L));
+                .andExpect(MockMvcResultMatchers.content().json(response));
+    }
+
+    private String readResourceFile(String fileName) throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:%s".formatted(fileName));
+        return Files.readString(resource.getFile().toPath());
     }
 }
+
+
+
+
+
+
+
+
+
